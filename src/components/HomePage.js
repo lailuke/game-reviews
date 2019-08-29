968e6a16da225e7ab877ebc7121cc1f3794358b7
Luke Lai <LukeL@meyer.com>
2019-08-29 09:41:15 -0700

    add all cards to home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,15 @@
 import React from "react";
+import CardList from "./CardList";
+import "../styles/home.css";
 
-const HomePage = () => <div>Home</div>;
+const HomePage = () => {
+  return (
+    <>
+      <CardList type={"PC"} />
+      <CardList type={"CON"} />
+      <CardList type={"VR"} />
+    </>
+  );
+};
 
 export default HomePage;
