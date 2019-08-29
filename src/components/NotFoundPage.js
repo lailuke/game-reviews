8bc5b94746f350ec0e62ea39c769e6ef0c3745b7
Luke Lai <LukeL@meyer.com>
2019-08-29 12:39:59 -0700

    TEST AXIOS

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,5 +1,17 @@
 import React from "react";
+import Axios from "axios";
 
-const NotFoundPage = () => <div>404</div>;
+const NotFoundPage = () => {
+  const buttonclick = () => {
+    Axios.get("https://api.github.com/users/mapbox").then(response => {
+      console.log(response.data);
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(response.headers);
+      console.log(response.config);
+    });
+  };
+  return <button onClick={buttonclick}>404</button>;
+};
 
 export default NotFoundPage;
