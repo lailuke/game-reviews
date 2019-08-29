4669640be72fa3bfaa6c718a4c379936a6ccb646
Luke Lai <LukeL@meyer.com>
2019-08-29 13:53:21 -0700

    shibe axios call

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,19 +1,37 @@
 import React from "react";
 import Axios from "axios";
 
-const NotFoundPage = () => {
-  const buttonclick = () => {
-    Axios.all([
-      Axios.get("https://api.github.com/users/mapbox"),
-      Axios.get("https://api.github.com/users/phantomjs")
-    ]).then(
-      Axios.spread((user1, user2) => {
-        console.log("Date created: ", user1.data.created_at);
-        console.log("Date created: ", user2.data.created_at);
-      })
-    );
+class NotFoundPage extends React.Component {
+  state = {
+    url: ""
   };
-  return <button onClick={buttonclick}>404</button>;
-};
+
+  buttonclick = () => {
+    const url =
+      "https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?count=1";
+    Axios.get(url, {
+      crossdomain: true
+    })
+      .then(response => {
+        this.setState(state => ({ url: response.data[0] }));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  toggleImage = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.buttonclick}>404</button>
+        <img src={this.state.url} alt="x" />
+      </>
+    );
+  }
+}
 
 export default NotFoundPage;
