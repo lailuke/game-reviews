ee9c5c6a352f2176106c95951732ce9d17e26733
Luke Lai <LukeL@meyer.com>
2019-08-29 08:42:21 -0700

    default state images

--- src/reducers/games.js
+++ src/reducers/games.js
@@ -32,55 +32,61 @@ const gamesDefaultState = [
   {
     id: "CON-1",
     name: "Halo 3",
-    category: "",
-    image: "",
-    link: "",
-    description: "",
+    category: "FPS",
+    image: "https://drive.google.com/uc?id=1OYy4fkvrQ1-zMNL1LrVUn25zadR4vpoe",
+    link: "https://www.google.com/",
+    description:
+      "A very good campaign and an entertaining variety of other things to do. The campaign has an excellent storyline with a lot of twists to keep you wanting more. The gameplay overall is very fun and keeps it fresh throughout the campaign, although there are many crazy glitches since it's an older game (2007). I love to mess around on the Forge mode. You can also re-watch your campaign and forge adventures in Theater and take screenshots and clips for your XBOX 360 Console. The multiplayer is okay, but it doesn't live up to Halo, Reach's multiplayer. The Armory is more limited on armor, but there are more colors than Reach and they are very vibrant.",
     rating: "https://drive.google.com/uc?id=1tl9sSPbQTROXJ5bV0stsBZbRaHI5nRhD"
   },
   {
     id: "CON-2",
-    name: "",
-    category: "",
-    image: "",
-    link: "",
-    description: "",
+    name: "Super Smash Bros. Ultimate",
+    category: "Fighting",
+    image: "https://drive.google.com/uc?id=1dt4z2JHJZbJffwVp7dz0aoUe5FOw6mja",
+    link: "https://www.google.com/",
+    description:
+      "This is one of those games where the creators really try to do their best to pack-in EVERYTHING in one tiny little cartridge for a standard price. The game has you start out with just EIGHT characters! If you've been caught up with the game and seen how it works, you already know that the character roster is INSANE. Hours upon hours will be spent unlocking everybody!",
     rating: "https://drive.google.com/uc?id=1tl9sSPbQTROXJ5bV0stsBZbRaHI5nRhD"
   },
   {
     id: "CON-3",
-    name: "",
-    category: "",
-    image: "",
-    link: "",
-    description: "",
+    name: "The Last of Us",
+    category: "Survival",
+    image: "https://drive.google.com/uc?id=1BeMcm54Ys-0s2DFzhfp1qmuoZInU7ln_",
+    link: "https://www.google.com/",
+    description:
+      "The Last Of Us. Is an amazing game with an perfect story that can make even the most hardcore gamer feel the intensity of the apocalyptic world. The graphics were breath taking, and were able to make you a very important part of this story, you choose their fate, whether they die or live is all on you. A game, to remember through the years with its stunning story.",
     rating: "https://drive.google.com/uc?id=1tl9sSPbQTROXJ5bV0stsBZbRaHI5nRhD"
   },
   {
     id: "VR-1",
-    name: "",
-    category: "",
-    image: "",
-    link: "",
-    description: "",
+    name: "Beat Saber",
+    category: "Musical Game",
+    image: "https://drive.google.com/uc?id=13TfNr6yjOcjANlGECUHlC1pPoXlhjIAM",
+    link: "https://www.google.com/",
+    description:
+      "I got my headset just for this game and it did not disappoint. Easily the best VR experience you can go for. I get motion sick very easily, so a lot of VR applications don't go so well for me, but since you're stationary for this, there's no motion sickness, and slowly improving and achieving higher and higher scores as well as pulling off difficult block chains is so satisfying. I seriously cannot recommend this game enough, especially on PC because you have the ability to mod the game and download custom songs.",
     rating: "https://drive.google.com/uc?id=1tl9sSPbQTROXJ5bV0stsBZbRaHI5nRhD"
   },
   {
     id: "VR-2",
-    name: "",
-    category: "",
-    image: "",
-    link: "",
-    description: "",
+    name: "Superhot VR",
+    category: "FPS",
+    image: "https://drive.google.com/uc?id=1-MGg6mjJ3B-5n-OIgxxrU9ynijOM3mS0",
+    link: "https://www.google.com/",
+    description:
+      "The best damn Matrix / John Wick experience you'll ever have! A delectable first-time VR experience that'll be hard to put down once your headset is on. Not as mysterious and story-driven as the original, but a fantastic demonstration of VR technology and fun. Add the Spybreak song from The Matrix into the mix, and you've got one hella of a good VR cupcake in front of ya.",
     rating: "https://drive.google.com/uc?id=1tl9sSPbQTROXJ5bV0stsBZbRaHI5nRhD"
   },
   {
     id: "VR-3",
-    name: "",
-    category: "",
-    image: "",
-    link: "",
-    description: "",
+    name: "Paranormal Activity: The Lost Soul",
+    category: "Horror",
+    image: "https://drive.google.com/uc?id=1qt_d7AoUSo25-g_mjRLFOcQQx3nhm8SN",
+    link: "https://www.google.com/",
+    description:
+      "I keep trying to beat it but I get scared very easily. I love it so much, just wish I wasn't such a chicken sh**. Great game, and an amazing experience.I recommend for those who want an adrenaline rush.",
     rating: "https://drive.google.com/uc?id=1tl9sSPbQTROXJ5bV0stsBZbRaHI5nRhD"
   }
 ];
